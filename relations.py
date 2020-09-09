11f9477e910e323a54bfe5e82d6221dcd1f13dd2
nithesh407 <nitheshravikumar13631@gmail.com>
2020-09-09 11:34:14 +0530

    relations

---
+++ relations.py
@@ -0,0 +1,10 @@
+# To print the relations
+name = input("enter the name:")
+if name =='haresh':
+    print('your brother name is nithesh')
+elif name =='karthi':
+    print("your son name is nithesh and haresh")
+else:
+    print("enter the correct name")        
+
+
